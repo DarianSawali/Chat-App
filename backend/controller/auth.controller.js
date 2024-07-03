01529c65ce80f842bc5c7ea0cf13019046a5fd74
Darian Sawali <darianaxel@gmail.com>
2024-07-02 23:11:29 -0700

    Update auth.controller.js

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,6 +1,16 @@
 export const signup = async(req, res) => {
     try{
         const {fullName, username, password, confirmPassword, gender} = req.body;
+
+        if(password !== confirmPassword) {
+            return res.status(400).json({error: "Passwords don't match"});
+        }
+
+        const user = await User.findOne({username});
+        if(user) {
+            return res.status(400).json({error:"Username already exists"});
+        }
+
     } catch (error) {
 
     }
