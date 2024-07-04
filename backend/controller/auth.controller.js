99a40298aa39baad9f1e4769d17641d27c8a0583
Darian Sawali <darianaxel@gmail.com>
2024-07-03 17:45:52 -0700

    new user update

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,3 +1,5 @@
+import User from "../models/user.model";
+
 export const signup = async(req, res) => {
     try{
         const {fullName, username, password, confirmPassword, gender} = req.body;
@@ -11,8 +13,28 @@ export const signup = async(req, res) => {
             return res.status(400).json({error:"Username already exists"});
         }
 
-    } catch (error) {
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
+        const newUser = new User({
+            fullName,
+            username,
+            password,
+            gender,
+            profilePic: gender=== "male" ? boyProfilePic : girlProfilePic
+        });
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic,
+        });
+
+    } catch (error) {
+        res.status(500).json({error:error.message});
     }
 }
 
