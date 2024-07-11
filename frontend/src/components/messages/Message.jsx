849d9fbd5ecaf1c914de01db698165a6521b44db
Darian Sawali <darianaxel@gmail.com>
2024-07-11 04:29:02 -0700

    no chat selection UI

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='chat chat-end'>
+    <div className='chat py-2 chat-end'>
         <div className='chat-image avatar'>
             <div className='w-8 rounded-full'>
                 <img 
@@ -11,9 +11,28 @@ const Message = () => {
             </div>
         </div>
         <div className='chat-bubble text-white bg-blue-500'>Hi! What's up?</div>
-        <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:42</div>
+        <div className='chat-footer opacity-50 text-xs flex gap-1 items-center pt-1'>12:42</div>
     </div>
   )
 }
 
 export default Message
+
+
+// const Message = () => {
+//     return (
+//       <div className='chat chat-end'>
+//           <div className='chat-image avatar'>
+//               <div className='w-8 rounded-full'>
+//                   <img 
+//                       src={"https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"} 
+//                       alt="Tailwind bubble chat bubble icon" />
+//               </div>
+//           </div>
+//           <div className='chat-bubble text-white bg-blue-500'>Hi! What's up?</div>
+//           <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:42</div>
+//       </div>
+//     )
+//   }
+  
+//   export default Message
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,19 +1,57 @@
 import React from 'react'
 import Messages from './Messages'
+import MessageInput from './MessageInput'
+import { TbMessages } from "react-icons/tb";
 
 const MessageContainer = () => {
+  const noChatSelected = true;
   return (
     <div className='md:min-w-[32rem] flex flex-col'>
-      <>
-      {/* Header */}
-        <div className='bg-slate-500 px-4 py-2 mb-2'>
-          <span className='label-text'>To:</span>{" "}
-          <span className='text-gray-900 font-bold'>Jonathan Doe</span>
-        </div>
-        <Messages />
-      </>
+      {noChatSelected ? <NoChatSelected /> : (
+        <>
+        {/* Header */}
+          <div className='bg-slate-500 px-4 py-2 mb-2'>
+            <span className='label-text'>To:</span>{" "}
+            <span className='text-gray-900 font-bold'>Jonathan Doe</span>
+          </div>
+          <Messages />
+          <MessageInput />
+        </>   
+      )}
     </div>
   )
 }
-
 export default MessageContainer
+
+const NoChatSelected = () => {
+  return (
+    <div className='flex items-center justify-center w-full h-full'>
+      <div className='flex items-center text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex-col gap-2'>
+        <p>Welcome!👋</p>
+        <p>Select a chat to start messaging</p>
+        <TbMessages className='text-3xl md:text-6x1 text-center'/>
+      </div>
+    </div>
+  )
+}
+// export default NoChatSelected
+
+
+
+// const MessageContainer = () => {
+//   return (
+//     <div className='md:min-w-[32rem] flex flex-col'>
+//       <>
+//       {/* Header */}
+//         <div className='bg-slate-500 px-4 py-2 mb-2'>
+//           <span className='label-text'>To:</span>{" "}
+//           <span className='text-gray-900 font-bold'>Jonathan Doe</span>
+//         </div>
+//         <Messages />
+//         </>
+//       <MessageInput />
+//     </div>
+//   )
+// }
+
+// export default MessageContainer
