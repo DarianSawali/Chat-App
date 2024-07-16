38f933b58374d4036571d21b8e6454e756448341
Darian Sawali <darianaxel@gmail.com>
2024-07-16 15:16:42 -0700

    sidebar search input function

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
+import { useAuthContext } from '../../context/AuthContext'
+import useConversation from '../../store/useConversation';
+import { extractTime } from '../../utils/extractTime.js';
 
-const Message = () => {
+const Message = ({message}) => {
+  const {authUser} = useAuthContext();
+  const {selectedConversation} = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const formattedTime = extractTime(message.createdAt);
+
+  const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? 'bg-purple-900' : '';
+
+
+
   return (
-    <div className='chat py-2 chat-end'>
+    <div className={`chat py-2 ${chatClassName}`}>
         <div className='chat-image avatar'>
             <div className='w-8 rounded-full'>
                 <img 
-                    src={"https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"} 
+                    src={profilePic} 
                     alt="Tailwind bubble chat bubble icon" />
             </div>
         </div>
-        <div className='chat-bubble text-white bg-blue-500'>Hi! What's up?</div>
-        <div className='chat-footer opacity-50 text-xs flex gap-1 items-center pt-1'>12:42</div>
+        <div className={`chat-bubble text-white pb-3 ${bubbleBgColor}`}>{message.message}</div>
+        <div className='chat-footer opacity-50 text-xs flex gap-1 items-center pt-1'>{formattedTime}</div>
     </div>
   )
 }
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoSearch } from "react-icons/io5";
+import useConversation from '../../store/useConversation';
+import useGetConversations from '../../hooks/useGetConversations';
+import toast from 'react-hot-toast';
 
 
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const {setSelectedConversation} = useConversation();
+  const { conversations } = useGetConversations();
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(!search) return;
+    if(search.length < 3){
+      return toast.error("Search term must be atleast 3 characters long")
+    }
+
+    const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()));
+
+    if(conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else toast.error("No such user found");
+  }
+
   return (
-    <form className='flex gap-2 items-start'>
-        <input type="text" placeholder='Search...' className='input input-bordered rounded-full'/>
+    <form onSubmit={handleSubmit} className='flex gap-2 items-start'>
+        <input type="text" placeholder='Search...' className='input input-bordered rounded-full'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
         <button type='submit' className='btn btn-circle border-transparent bg-purple-900 text-white hover:bg-transparent hover:border-white flex items-center justify-center'>
             <IoSearch className='w-6 h-6 outline-none'/>
         </button>
